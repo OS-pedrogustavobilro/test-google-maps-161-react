45accf6dac95cc05957ad6cbb5dce4ca4c34745e
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2026-01-05 13:19:00 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'test-google-maps-161',
+  webDir: 'dist'
+};
+
+export default config;
