77bdea2ae0be912319c95896ce10be62ccd51524
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2026-01-05 13:37:16 +0000

    Add test for google maps with useIonViewDidEnter

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,48 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  useIonViewDidEnter
+} from '@ionic/react';
+import { GoogleMap } from '@capacitor/google-maps';
+import { useRef } from 'react';
 import './Home.css';
 
 const Home: React.FC = () => {
+  const mapRef = useRef<HTMLElement | null>(null);
+
+  useIonViewDidEnter(() => {
+    const initMap = async () => {
+      if (!mapRef.current) return;
+
+      await GoogleMap.create({
+        id: 'test-map',
+        element: mapRef.current,
+        apiKey: 'VALID_API_KEY',
+        config: {
+          center: { lat: 33.6, lng: -117.9 },
+          zoom: 8
+        }
+      });
+
+      console.log('Map created successfully');
+    };
+
+    void initMap();
+  });
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Map</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+
+      <IonContent scrollY={false}>
+        <capacitor-google-map ref={mapRef}></capacitor-google-map>
       </IonContent>
     </IonPage>
   );
